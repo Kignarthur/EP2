c4a8310a840564574cda447562039570b7e56e9c
Arthur Teixeira <amarthur@usp.br>
2020-06-09 17:03:59 -0300

    Add function to check wills cards
    This function checks if wills cards together can form
    Kaprekars number

--- EP2_4876102.jl
+++ EP2_4876102.jl
@@ -15,3 +15,23 @@ function checkKaprekar()
     @test checkWill([0,0,0,0,0,0,0,0,0,0,0,0,0,6,1,7,4]) == true
     println("OK")
 end
+
+function checkWill(cards)
+
+    v = zeros(Int, 10)
+
+    for index in 1 : length(cards)
+        if cards[index] != 0
+            v[cards[index]] += 1
+        end
+    end
+
+    if v[1] == 0 || v[4] == 0 || v[6] == 0 || v[7] == 0
+        return false
+    else
+        return true
+    end
+
+end
+
+checkKaprekar()
