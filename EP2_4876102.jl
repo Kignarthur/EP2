998040495e50beb494dcef9b69a5bc79690845fa
Arthur Teixeira <amarthur@usp.br>
2020-06-09 18:11:20 -0300

    Create test function for jackson number

--- EP2_4876102.jl
+++ EP2_4876102.jl
@@ -34,6 +34,23 @@ function checkTakiNumber()
     println("OK")
 end
 
+function checkJacksonNumber()
+    @test checkJackson(3, [0, 1]) == false
+    @test checkJackson(14, [1, 4, 6, 7, 1, 1, 7, 7]) == false
+    @test checkJackson(15, [1, 1, 7, 7, 1, 0, 0, 0]) == false
+    @test checkJackson(92[0, 1, 2, 3, 4, 5, 7, 7, 8, 9]) == false
+    @test checkJackson(65,[1, 2, 3, 4, 5, 6, 7, 8, 9]) == false
+    @test checkJackson(35,[0, 4, 0, 7]) == false
+    @test checkJackson(89,[1, 9, 6, 9]) == false
+    @test checkJackson(79,[7, 4, 6, 7]) == false
+    @test checkJackson(323846264,[1, 1, 7, 7]) == false
+    @test checkJackson(31415, [1, 3 ,6, 5, 1, 1, 7, 4, 7, 7, 1, 1, 1, 2, 3, 4, 4]) == true
+    @test checkJackson(0[0, 1, 4, 7, 1, 4, 7, 1, 4, 7, 1, 6, 0]) == true
+    @test checkJackson(123456789[1, 2, 3, 4, 5, 6, 7, 8, 9,1, 6, 7, 1, 4, 6, 7, 7, 1, 9]) == true
+    @test checkJackson(227[7,7,1,1,0,0,0,2,0,2,0,7,0,0,0,0,6,1,7,4]) == true
+    println("OK")
+end
+
 function checkWill(cards)
 
     v = zeros(Int, 10)
